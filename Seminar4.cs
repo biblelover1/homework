0c185f98a11d145e7c80335a8af41b04383aa03b
Evgeniy <evgenijkopylov19540@gmail.com>
2022-12-12 15:12:01 +0300

    HW4

---
+++ Seminar4.cs
@@ -0,0 +1,58 @@
+﻿//HW Seminar 4 Exercise 1: Напишите цикл, который принимает на вход два числа (A и B) 
+//              и возводит число A в натуральную степень B.(Math.Pow использовать нельзя!!!)
+
+// Console.WriteLine("Enter base: ");
+// int baseNumb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter grade: ");
+// int gradeNumb = Convert.ToInt32(Console.ReadLine());
+// int res = Grade(baseNumb, gradeNumb);
+// Console.WriteLine($"Result is {res}");
+
+// int Grade(int a, int b){
+//     int sum = 2;
+//     for(int i = 2; i <= b; i++){
+//         a = a*sum;
+//     }
+//     return a;
+// }
+
+
+// Exercise 2: Напишите программу, которая 
+//  принимает на вход число и выдаёт сумму цифр в числе.(Массивы и строки использовать нельзя!!!)
+
+// void Sum(int num){
+//     int sum = 0;
+//     int a = 0;
+//     while(num > 0)
+//     {
+//         a = num % 10;
+//         sum = sum + a;
+//         num = num / 10;
+//     }
+//         Console.WriteLine($"Sum of signs is {sum}");
+// }
+// Console.WriteLine("Enter number: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// Sum(numb);
+
+
+//Exercise 3: Напишите программу, 
+//которая задаёт массив из произвольного кол-ва элементов и выводит их на экран.
+
+// Console.WriteLine("Enter number ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[numb];
+// ShowArray(array, numb);
+
+// void ShowArray(int[] arr, int length){
+//     int lenght = arr.Length;
+//     for(int i = 0; i < lenght; i++){
+//         arr[i] = new Random().Next(0, 9);
+//         Console.Write($"{arr[i]}, ");
+//     }
+// }
+
+
+
+
+ 
